99438c7fd4cf70cf217bf70c1859817f8d99ef6b
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-08-14 09:48:22 -0700

    Initial Commit

---
+++ SilkMultiSDLBrokey/Program.cs
@@ -0,0 +1,34 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Sdl;
+
+SdlWindowing.Use();
+
+WindowOptions options = WindowOptions.Default;
+
+IWindow       window = Window.Create(options);
+IInputContext input;
+
+window.Load += delegate {
+	input = window.CreateInput();
+};
+
+window.Update += delegate {
+	window.Close();
+	window.Dispose();
+};
+
+window.Run();
+
+window = Window.Create(options);
+
+window.Load += delegate {
+	input = window.CreateInput();
+};
+
+window.Update += delegate {
+	window.Close();
+	window.Dispose();
+};
+
+window.Run();
